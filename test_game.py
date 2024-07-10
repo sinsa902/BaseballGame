331033c26f274c3a41e22bb8265a5e928fbcfd99
gyu.byeon <isoul902@gmail.com>
2024-07-10 15:23:11 +0900

    [UPDATE] REFACTORING PAGE2

--- test_game.py
+++ test_game.py
@@ -7,10 +7,15 @@ class TestBaseballGame(TestCase):
     def setUp(self):
         self.game = BaseballGame()
         super().setUp()
-    def test_exception_when_input_is_none(self):
-        with self.assertRaises(Exception) as context:
-            self.game.guess(None)
 
-    def test_exception_when_input_is_unmatched(self):
-        with self.assertRaises(Exception) as context:
-            self.game.guess("12")
+    def assert_illegal_argument(self, guessNumber):
+        try:
+            self.game.guess(guessNumber)
+            self.fail()
+        except Exception as exception:
+            pass
+
+    def test_exception_when_invalid_input(self):
+        self.assert_illegal_argument(None)
+        self.assert_illegal_argument("12")
+        self.assert_illegal_argument("1234")
