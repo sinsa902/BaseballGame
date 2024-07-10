62f1aee2b344b46707e0cdc7cf2523223f5251ab
gyu.byeon <isoul902@gmail.com>
2024-07-10 15:31:19 +0900

    도중 멈췄음

--- game.py
+++ game.py
@@ -1,7 +1,11 @@
 class BaseballGame:
     def guess(self, guessNumber):
-        if not guessNumber:
+        if guessNumber is None:
             raise TypeError()
 
-        if len(guessNumber) !=3:
+        if len(guessNumber) != 3:
             raise TypeError()
+
+        for number in guessNumber:
+            if not ord("0") <= ord(number) <= ord("9"):
+                raise TypeError(
--- test_game.py
+++ test_game.py
@@ -12,10 +12,12 @@ class TestBaseballGame(TestCase):
         try:
             self.game.guess(guessNumber)
             self.fail()
-        except Exception as exception:
+        except TypeError as exception:
             pass
 
     def test_exception_when_invalid_input(self):
         self.assert_illegal_argument(None)
         self.assert_illegal_argument("12")
         self.assert_illegal_argument("1234")
+        self.assert_illegal_argument("12s")
+        self.assert_illegal_argument("121")
